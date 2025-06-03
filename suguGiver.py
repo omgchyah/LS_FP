42fc7b7ebbed6804c4a5acc37f51b64c97c53fb7
Rossana Liendo <rossana.liendo@gmail.com>
2025-06-03 15:18:05 +0200

    Created a new Python Script

---
+++ suguGiver.py
@@ -0,0 +1,27 @@
+answer = input("Welcome. You must answer correctly each question with either true(t) or false(f). Is Python a free language? (f/t)").lower()
+
+answers = []
+
+while answer != "f" and answer != "t":
+    answer = input("Choose either 't' or 'f'").lower()
+
+answer[] = answer
+
+
+    
+
+    
+    if answer == "t":
+            print("Congrats!")
+    else:
+            print("Try again!")
+    
+    
+
+
+#give numbers to asnwers, save key with answer, compare keys and make a array of those that are different
+
+#show user the questions of the questions he had wrong and answer
+
+
+
