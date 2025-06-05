8c0d11cdaa3db4c4cc00b87b4533421dcb8c66f1
Rossana Liendo <rossana.liendo@gmail.com>
2025-06-05 11:43:50 +0200

    Working on the code

--- suguGiver.py
+++ suguGiver.py
@@ -1,27 +1,51 @@
-answer = input("Welcome. You must answer correctly each question with either true(t) or false(f). Is Python a free language? (f/t)").lower()
+# Create 3 arrays
 
-answers = []
+questions = ["Python uses indentation to define blocks", "The keyword 'def' is used to define a function in Python", "In Python, lists are immutable (They cannot be changed after creation)", "The print() function is used to display output to the console", "Python variable names can start with a number"]
 
-while answer != "f" and answer != "t":
-    answer = input("Choose either 't' or 'f'").lower()
+answers = ["t", "t", "f", "t", "f"]
 
-answer[] = answer
+userAnswers = []
 
+print("Welcome. Read each statement carefully. You must choose either true(t) or false(f)")
 
-    
+#print out questions and save answers in an array
 
-    
-    if answer == "t":
-            print("Congrats!")
-    else:
-            print("Try again!")
-    
-    
+i = 0
 
+while len(userAnswers) < 5:
+        answer = ""
+        answer = input(questions[i]).lower()   
+        while answer != "f" and answer != "t":
+                answer = input("You have to type either 't' or 'f'" + questions[i]).lower()   
+        i += 1
+        userAnswers.append(answer)
 
-#give numbers to asnwers, save key with answer, compare keys and make a array of those that are different
+#Compare both arrays, select those that differ
 
-#show user the questions of the questions he had wrong and answer
+incorrectQuestions = []
+incorrectAnswers = []
+
+for i in range(len(answers)):
+        if answer != userAnswers[i]:
+                incorrectAnswers.append(userAnswers[i])
+                incorrectQuestions.append(questions[i])
+                i += 1
+
+# Print out a response for the user and the incorrect questions/answer if there are any
+
+if len(incorrectAnswers) == 0:
+        response = "Congrats! You'got every question right! You've won a Sugu!"
+elif len(incorrectAnswers) == 1:
+        response = "Sorry! You this question wrong: " + incorrectQuestions[0], incorrectAnswers[0]
+elif len(incorrectAnswers) == len(questions):
+        response = "You've got every question wrong! Try again!"
+else:
+        for i in range(len(incorrectQuestions)):
+                response = ("Sorry! You've got these questions wrong: " + incorrectQuestions[i] + incorrectAnswers[i])
+                
+
+print(response)
+
 
 
 
