b94fccbe9772c4aa858b007fac96058a21ab851d
Rossana Liendo <rossana.liendo@gmail.com>
2025-06-05 14:24:12 +0200

    Changed case to snake case

--- suguGiver.py
+++ suguGiver.py
@@ -4,7 +4,7 @@ questions = ["Python uses indentation to define blocks", "The keyword 'def' is u
 
 answers = ["t", "t", "f", "t", "f"]
 
-userAnswers = []
+user_answers = []
 
 print("Welcome. Read each statement carefully. You must choose either true(t) or false(f)")
 
@@ -12,36 +12,36 @@ print("Welcome. Read each statement carefully. You must choose either true(t) or
 
 i = 0
 
-while len(userAnswers) < 5:
+while len(user_answers) < len(answers):
         answer = ""
         answer = input(questions[i]).lower()   
         while answer != "f" and answer != "t":
-                answer = input("You have to type either 't' or 'f'" + questions[i]).lower()   
+                answer = input("You have to type either 't' or 'f'. " + questions[i]).lower()   
         i += 1
-        userAnswers.append(answer)
+        user_answers.append(answer)
 
 #Compare both arrays, select those that differ
 
-incorrectQuestions = []
-incorrectAnswers = []
+incorrect_questions = []
+incorrect_answers = []
 
 for i in range(len(answers)):
-        if answer != userAnswers[i]:
-                incorrectAnswers.append(userAnswers[i])
-                incorrectQuestions.append(questions[i])
-                i += 1
+        if answers[i] != user_answers[i]:
+                incorrect_answers.append(user_answers[i])
+                incorrect_questions.append(questions[i])
 
 # Print out a response for the user and the incorrect questions/answer if there are any
 
-if len(incorrectAnswers) == 0:
-        response = "Congrats! You'got every question right! You've won a Sugu!"
-elif len(incorrectAnswers) == 1:
-        response = "Sorry! You this question wrong: " + incorrectQuestions[0], incorrectAnswers[0]
-elif len(incorrectAnswers) == len(questions):
+if len(incorrect_answers) == 0:
+        response = "Congrats! You've got every question right! You've won a Sugu!"
+elif len(incorrect_answers) == 1:
+        response = "Sorry! You this question wrong: " + incorrect_questions[0] + " Yo answer: " + incorrect_answers[0]
+elif len(incorrect_answers) == len(questions):
         response = "You've got every question wrong! Try again!"
 else:
-        for i in range(len(incorrectQuestions)):
-                response = ("Sorry! You've got these questions wrong: " + incorrectQuestions[i] + incorrectAnswers[i])
+        for i in range(len(incorrect_questions)):
+                response = ("Sorry! You've got these questions wrong: "
+                + incorrect_questions[i] + incorrect_answers[i])
                 
 
 print(response)
